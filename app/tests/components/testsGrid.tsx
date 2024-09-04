99f3ec74c4254573843a6f1a99428d0b37282b10
fellowfy-fy <zhoraawesome@gmail.com>
2024-09-04 16:55:34 +0300

    Добавил отметку "зеленой галочки"

--- app/tests/components/testsGrid.tsx
+++ app/tests/components/testsGrid.tsx
@@ -1,12 +1,22 @@
 import styles from "../styles/testsGrid.module.css";
+import Image from "next/image";
 
 export default function TestsGrid() {
-  const tests = 28;
+  const tests: number = 28;
 
   return (
     <div className={styles.grid_container}>
       {Array.from({ length: tests }, (_, i) => (
         <div key={i} className={styles.grid_item}>
+          <div className={styles.checked}>
+            <Image
+              src="/checked.svg"
+              alt="Arrow"
+              width={19.5}
+              height={15.5}
+              className={styles.arrow}
+            />
+          </div>
           {i + 1}
         </div>
       ))}
