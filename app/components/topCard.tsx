e5912dde9aae9052acc0892b62bb204f42171515
terrxrmachine <so3.workspace@gmail.com>
2024-09-04 17:21:48 +0400

    топ кард

--- app/components/topCard.tsx
+++ app/components/topCard.tsx
@@ -1,9 +1,52 @@
-import Image from "next/image";
-import styles from './styles/TopCard.module.css'; 
+import type { NextPage } from "next";
+import PropTypes from 'prop-types';  // <-- Import PropTypes
+import styles from "../styles/TopCard.module.css";
 
-export default function TopCard() {
-    return(
-    <div>
+const TopCard = ({ className = "" }) => {
+  return (
+    <div className={[styles.topCard, className].join(" ")}>
+      <div className={styles.content}>
+        <img
+          className={styles.contentChild}
+          loading="lazy"
+          alt=""
+          src="/group-861.svg"
+        />
+        <div className={styles.categories}>
+          <div className={styles.parent}>
+            <a className={styles.a}>Learning</a>
+            <div className={styles.div}>Category В</div>
+          </div>
+        </div>
+        <div className={styles.progressIndicator}>
+          <img
+            className={styles.filledChevronCircleRight}
+            loading="lazy"
+            alt=""
+            src="/4--filled--chevroncircleright.svg"
+          />
+        </div>
+      </div>
+      <div className={styles.questionsOutOf730PassedParent}>
+        <div className={styles.questionsOutOf}>
+          112 questions out of 730 passed
+        </div>
+        <div className={styles.progressBar}>
+          <div className={styles.progressSteps} />
+          <div className={styles.progressSteps1} />
+          <div className={styles.progressSteps2} />
+          <div className={styles.progressSteps3} />
+          <div className={styles.progressSteps4} />
+          <div className={styles.progressSteps5} />
+          <div className={styles.progressSteps6} />
+        </div>
+      </div>
     </div>
-    );
+  );
 };
+
+TopCard.propTypes = {
+  className: PropTypes.string,
+};
+
+export default TopCard;
